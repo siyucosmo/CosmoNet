be09900298b500b3ebdca89b469abb58c475b86d
Siyu He <siyuh@gpu2.int.autonlab.org>
2017-10-18 03:08:57 -0400

    1.more shuffle in the input

--- io_Cosmo.py
+++ io_Cosmo.py
@@ -57,7 +57,8 @@ def read_tfrecord(filename_queue):
     NbodySimuDecode = tf.decode_raw(parsed_example['data_raw'],tf.float64)
     labelDecode = tf.decode_raw(parsed_example['label_raw'],tf.float64)
     NbodySimus = tf.reshape(NbodySimuDecode,[64,64,64])
-    
+     
+    #augment 
     NbodySimus = tf.cond(tf.random_uniform([1],maxval=1)[0] < tf.constant(.5),lambda:NbodySimus,lambda:NbodySimus[::-1,:,...])
     NbodySimus = tf.cond(tf.random_uniform([1],maxval=1)[0] < tf.constant(.5),lambda:NbodySimus,lambda:NbodySimus[:,::-1,...])
     NbodySimus = tf.cond(tf.random_uniform([1],maxval=1)[0] < tf.constant(.5),lambda:NbodySimus,lambda:NbodySimus[:,:,::-1])
@@ -69,23 +70,26 @@ def read_tfrecord(filename_queue):
     NbodySimus = tf.cond(tf.logical_and(prand < tf.constant(4./6) , prand > tf.constant(3./6)), lambda:tf.transpose(NbodySimus, perm = (2,0,1)),lambda:NbodySimus)
     NbodySimus = tf.cond(tf.logical_and(prand < tf.constant(5./6) , prand > tf.constant(4./6)), lambda:tf.transpose(NbodySimus, perm = (2,1,0)),lambda:NbodySimus)
     
+    #normalize
+    NbodySimus /= (tf.reduce_sum(NbodySimus)/64**3+0.)
     NbodySimuAddDim = tf.expand_dims(NbodySimus,axis = 3)
     label = tf.reshape(labelDecode,[2])
+    label = (label - tf.constant([2.995679839999998983e-01,8.610806619999996636e-01],dtype = tf.float64))/tf.constant([2.905168635566176411e-02,4.023372385668218254e-02],dtype = tf.float64)
     return NbodySimuAddDim,label
     
 def readDataSet(filenames):
     filename_queue = tf.train.string_input_producer(filenames,num_epochs=None,shuffle=True)
-    NbodySimus,label= read_tfrecord(filename_queue)
-    NbodySimus_batch, label_batch = tf.train.shuffle_batch(
-    	[NbodySimus,label],
+    #NbodySimus,label= read_tfrecord(filename_queue)
+    NbodyList = [read_tfrecord(filename_queue) for _ in range(hyper_parameters_Cosmo.Input["NUM_THREADS"])]
+    NbodySimus_batch, label_batch = tf.train.shuffle_batch_join(
+    	#[NbodySimus,label],
+	NbodyList,
     	batch_size = hyper_parameters_Cosmo.Input["BATCH_SIZE"],
-    	num_threads = hyper_parameters_Cosmo.Input["NUM_THREADS"],
+    	#num_threads = hyper_parameters_Cosmo.Input["NUM_THREADS"],
     	capacity = hyper_parameters_Cosmo.Input["CAPACITY"],
-    	min_after_dequeue = hyper_parameters_Cosmo.Input["MIN_AFTER_DEQUEUE"])
+    	min_after_dequeue = hyper_parameters_Cosmo.Input["MIN_AFTER_DEQUEUE"],
+	allow_smaller_final_batch=True)
     
-    #normalize
-    NbodySimus_batch /= (tf.reduce_sum(NbodySimus_batch, axis=(1,2,3))/(64**3)+.0)[:,None,None,None,:]
-    label_batch = (label_batch-tf.constant([2.995679839999998983e-01,8.610806619999996636e-01],dtype = tf.float64))/tf.constant([2.905168635566176411e-02,4.023372385668218254e-02],dtype = tf.float64)
     return  NbodySimus_batch, label_batch
     
     
