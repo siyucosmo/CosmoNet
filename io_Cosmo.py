fdbcc1c36a292a41d23d915848615002f512439a
Siyu He <siyuh@gpu2.int.autonlab.org>
2017-10-26 14:13:38 -0400

    add test

--- io_Cosmo.py
+++ io_Cosmo.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import hyper_parameters_Cosmo
 import os
+import itertools
 from data_aug import *
 
 def _float64_feature(value):
@@ -57,7 +58,7 @@ def read_tfrecord(filename_queue):
     NbodySimuDecode = tf.decode_raw(parsed_example['data_raw'],tf.float64)
     labelDecode = tf.decode_raw(parsed_example['label_raw'],tf.float64)
     NbodySimus = tf.reshape(NbodySimuDecode,[64,64,64])
-     
+        
     #augment 
     NbodySimus = tf.cond(tf.random_uniform([1],maxval=1)[0] < tf.constant(.5),lambda:NbodySimus,lambda:NbodySimus[::-1,:,...])
     NbodySimus = tf.cond(tf.random_uniform([1],maxval=1)[0] < tf.constant(.5),lambda:NbodySimus,lambda:NbodySimus[:,::-1,...])
@@ -80,7 +81,7 @@ def read_tfrecord(filename_queue):
 def readDataSet(filenames):
     filename_queue = tf.train.string_input_producer(filenames,num_epochs=None,shuffle=True)
     NbodySimus,label= read_tfrecord(filename_queue)
-    NbodyList = [read_tfrecord(filename_queue) for _ in range(hyper_parameters_Cosmo.Input["NUM_THREADS"])]
+    #NbodyList = [read_tfrecord(filename_queue) for _ in range(hyper_parameters_Cosmo.Input["NUM_THREADS"])]
     NbodySimus_batch, label_batch = tf.train.shuffle_batch(
     	[NbodySimus,label],
 	#NbodyList,
@@ -91,8 +92,61 @@ def readDataSet(filenames):
 	allow_smaller_final_batch=True)
     
     return  NbodySimus_batch, label_batch
+
+
+def read_test_tfrecord(filename_queue):
+    reader = tf.TFRecordReader()
+    _,single_example = reader.read(filename_queue)
+    parsed_example = tf.parse_single_example(
+    single_example,
+    features = {
+        "data_raw": tf.FixedLenFeature([],tf.string),
+        "label_raw": tf.FixedLenFeature([],tf.string)
+    }
+    )
+
+    NbodySimuDecode = tf.decode_raw(parsed_example['data_raw'],tf.float64)
+    labelDecode = tf.decode_raw(parsed_example['label_raw'],tf.float64)
+    NbodySimus = tf.reshape(NbodySimuDecode,[64,64,64])
+    NbodySimus /= (tf.reduce_sum(NbodySimus)/64**3+0.)
+    NbodySimuAddDim = tf.expand_dims(NbodySimus,3)
+    label = tf.reshape(labelDecode,[2])
+    labelAddDim = (label - tf.constant([2.995679839999998983e-01,8.610806619999996636e-01],dtype = tf.float64))/tf.constant([2.905168635566176411e-02,4.023372385668218254e-02],dtype = tf.float64)
+    print NbodySimuAddDim.shape
+    '''        
+    NbodySimuAddDimList = []
+    #augment 
+    for trans in itertools.permutations([0,1,2]):
+	NbodySimuTrans = tf.transpose(NbodySimus, perm = tuple(trans))
+	for flips in list(itertools.product([True, False], repeat=3)):
+		Nbody = tf.identity(NbodySimuTrans)
+		if flips[0]: Nbody = Nbody[::-1,...]
+                if flips[1]: Nbody = Nbody[:,::-1,...]
+                if flips[2]: Nbody = Nbody[:,:,::-1]
+		NbodySimuAddDimList.append(tf.expand_dims(Nbody,0))
     
+    NbodySimuAddDim = tf.concat(NbodySimuAddDimList,0);
+    #normalize
+    label = tf.reshape(labelDecode,[2])
+    label = (label - tf.constant([2.995679839999998983e-01,8.610806619999996636e-01],dtype = tf.float64))/tf.constant([2.905168635566176411e-02,4.023372385668218254e-02],dtype = tf.float64)
+    labelAddDim = tf.tile(tf.expand_dims(label,0),tf.constant([48,1]))
+    '''
+    return NbodySimuAddDim,labelAddDim
     
+def readTestSet(filenames):
+    filename_queue = tf.train.string_input_producer(filenames,num_epochs=None,shuffle=False)
+    NbodySimus,label= read_test_tfrecord(filename_queue)
+    NbodySimus_batch, label_batch = tf.train.batch(
+        [NbodySimus,label],
+        #NbodyList,
+        batch_size = hyper_parameters_Cosmo.Input_Test["BATCH_SIZE"],
+        num_threads = hyper_parameters_Cosmo.Input_Test["NUM_THREADS"],
+        capacity = hyper_parameters_Cosmo.Input_Test["CAPACITY"],
+	enqueue_many=False,
+        allow_smaller_final_batch=True)
+
+    return  NbodySimus_batch, label_batch
+       
         
 
 if __name__ == '__main__':
@@ -100,7 +154,7 @@ if __name__ == '__main__':
     
     label_path = os.path.join('/zfsauton/home/siyuh/256_64','basics_infos_1000_1499.txt')
     labels = np.loadtxt(label_path,delimiter=',')    
-    
+    '''   
     for i in range(0,499):
         data = []
         label = []
@@ -111,26 +165,18 @@ if __name__ == '__main__':
             data = np.append(data,np.load(data_path))
             label = np.append(label,labels[numDirectory][[1,3]])
         loadNpyData(data.reshape(-1,64,64,64,1),label.reshape(-1,2),i).convert_to()
-    
     '''
-    NbodySimu, NbodyLabel,prand = readDataSet(filenames=["0.tfrecord"])
+   
+    NbodySimu, NbodyLabel = readTestSet(filenames=["400.tfrecord","401.tfrecord"])
     with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
         coord = tf.train.Coordinator();
         threads = tf.train.start_queue_runners(coord=coord);
-        for i in range(10):
-            _NbodySimu,_NbodyLabel = sess.run([NbodySimu, NbodyLabel]);
-            print _prand
+        for i in range(65):
+            _NbodySimu,_NbodyLabel = sess.run([NbodySimu, NbodyLabel])
+	    print (_NbodySimu[0])
             print ("----------------");
 	
 	coord.request_stop()
 	coord.join()
-    '''
-    
-    
-    
-
-
-
-
-
+  
